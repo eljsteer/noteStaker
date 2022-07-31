a5060ef9a71969ee57a5159270366bd8226f17b8
Jason <el.jsteer@gmail.com>
2022-07-31 22:16:26 +1000

    update for askBCS

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -104,7 +104,6 @@ const handleNoteView = (e) => {
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
-  e.preventDefault();
   activeNote = {};
   renderActiveNote();
 };
